5a548e9df764f9452f463f86674510c76e505a1c
nicolevichez <nicolevichez@gmail.com>
2019-06-13 13:16:27 -0500

    created my first project

---
+++ src/com/fundamentals/java/Main.java
@@ -0,0 +1,13 @@
+package com.fundamentals.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+ System.out.println("Welcome to java");
+
+    }
+}
+
+
+
